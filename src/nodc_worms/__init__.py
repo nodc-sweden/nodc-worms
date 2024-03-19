20e1ad3e1cdfa40b184432f700ac712b6931b941
Wenzer Magnus <a001985@ad.smhi.se>
2024-03-19 16:42:20 +0100

    Fix config

--- src/nodc_worms/__init__.py
+++ src/nodc_worms/__init__.py
@@ -6,10 +6,18 @@ import importlib.resources
 from .taxa_worms import TaxaWorms
 
 
-TAXA_WORMS_FILE_PATH = importlib.resources.files("CONFIG_FILES") / "taxa_worms.txt"
+# TAXA_WORMS_FILE_PATH = importlib.resources.files("src/nodc_worms/CONFIG_FILES") / "taxa_worms.txt"
+
+if getattr(sys, 'frozen', False):
+    THIS_DIR = pathlib.Path(sys.executable).parent
+else:
+    THIS_DIR = pathlib.Path(__file__).parent
+
+TAXA_WORMS_FILE_PATH = THIS_DIR / "CONFIG_FILES" / "taxa_worms.txt"
 
 
 @functools.cache
 def get_taxa_worms_object() -> "TaxaWorms":
+
     return TaxaWorms(str(TAXA_WORMS_FILE_PATH))
 
