c98ebee9dff57482bcff21d1b1f8136ad3fafb2b
Wenzer Magnus <a001985@ad.smhi.se>
2024-03-25 13:18:23 +0100

    Added download config func

--- src/nodc_worms/__init__.py
+++ src/nodc_worms/__init__.py
@@ -1,23 +1,36 @@
 import functools
-import sys
+import logging
 import pathlib
-import importlib.resources
 
-from .taxa_worms import TaxaWorms
+import requests
+from nodc_worms.taxa_worms import TaxaWorms
 
+logger = logging.getLogger(__name__)
 
-# TAXA_WORMS_FILE_PATH = importlib.resources.files("src/nodc_worms/CONFIG_FILES") / "taxa_worms.txt"
+THIS_DIR = pathlib.Path(__file__).parent
+CONFIG_DIR = THIS_DIR / "CONFIG_FILES"
 
-if getattr(sys, 'frozen', False):
-    THIS_DIR = pathlib.Path(sys.executable).parent
-else:
-    THIS_DIR = pathlib.Path(__file__).parent
-
-TAXA_WORMS_FILE_PATH = THIS_DIR / "CONFIG_FILES" / "taxa_worms.txt"
+CONFIG_URLS = [
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-worms/main/src/nodc_worms/CONFIG_FILES/taxa_worms.txt',
+]
 
 
 @functools.cache
 def get_taxa_worms_object() -> "TaxaWorms":
+    taxa_worms_config_path = CONFIG_DIR / "taxa_worms.txt"
+    return TaxaWorms(str(taxa_worms_config_path))
 
-    return TaxaWorms(str(TAXA_WORMS_FILE_PATH))
+
+def update_config_files() -> None:
+    """Downloads config files from github"""
+    try:
+        for url in CONFIG_URLS:
+            name = pathlib.Path(url).name
+            target_path = CONFIG_DIR / name
+            res = requests.get(url)
+            with open(target_path, 'w', encoding='utf8') as fid:
+                fid.write(res.text)
+                logger.info(f'Config file "{name}" updated from {url}')
+    except requests.exceptions.ConnectionError:
+        logger.warning('Connection error. Could not update config files!')
 
