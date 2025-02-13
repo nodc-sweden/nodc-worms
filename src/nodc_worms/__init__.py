603011ddedf29bfaf054e5c84ece1a719d8ddd34
a001985 <magnus.wenzer@smhi.se>
2025-02-13 11:11:36 +0100

    New paths

--- src/nodc_worms/__init__.py
+++ src/nodc_worms/__init__.py
@@ -2,15 +2,14 @@ import functools
 import logging
 import os
 import pathlib
-import ssl
 
-import requests
 from nodc_worms.taxa_worms import TaxaWorms
 from nodc_worms.translate_worms import TranslateDyntaxa
 
 logger = logging.getLogger(__name__)
 
-CONFIG_SUBDIRECTORY = 'nodc_worms'
+CONFIG_ENV = 'NODC_CONFIG'
+
 CONFIG_FILE_NAMES = [
     'taxa_worms.txt',
     'translate_to_worms.txt',
@@ -18,48 +17,21 @@ CONFIG_FILE_NAMES = [
 
 
 CONFIG_DIRECTORY = None
-if os.getenv('NODC_CONFIG'):
-    CONFIG_DIRECTORY = pathlib.Path(os.getenv('NODC_CONFIG')) / CONFIG_SUBDIRECTORY
-TEMP_CONFIG_DIRECTORY = pathlib.Path.home() / 'temp_nodc_config' / CONFIG_SUBDIRECTORY
+if os.getenv(CONFIG_ENV) and pathlib.Path(os.getenv(CONFIG_ENV)).exists():
+    CONFIG_DIRECTORY = pathlib.Path(os.getenv(CONFIG_ENV))
 
-
-CONFIG_URL = r'https://raw.githubusercontent.com/nodc-sweden/nodc_config/refs/heads/main/' + f'{CONFIG_SUBDIRECTORY}/'
-
-
-def get_config_path(name: str) -> pathlib.Path:
+def get_config_path(name: str = None) -> pathlib.Path:
+    if not CONFIG_DIRECTORY:
+        raise NotADirectoryError(f'Config directory not found. Environment path {CONFIG_ENV} does not seem to be set.')
+    if not name:
+        return CONFIG_DIRECTORY
     if name not in CONFIG_FILE_NAMES:
         raise FileNotFoundError(f'No config file with name "{name}" exists')
-    if CONFIG_DIRECTORY:
-        path = CONFIG_DIRECTORY / name
-        if path.exists():
-            return path
-    temp_path = TEMP_CONFIG_DIRECTORY / name
-    if temp_path.exists():
-        return temp_path
-    update_config_file(temp_path)
-    if temp_path.exists():
-        return temp_path
-    raise FileNotFoundError(f'Could not find config file {name}')
+    path = CONFIG_DIRECTORY / name
+    if not path.exists():
+        raise FileNotFoundError(f'Could not find config file {name}')
+    return path
 
-
-def update_config_file(path: pathlib.Path) -> None:
-    path.parent.mkdir(exist_ok=True, parents=True)
-    url = CONFIG_URL + path.name
-    try:
-        res = requests.get(url, verify=ssl.CERT_NONE)
-        with open(path, 'w', encoding='utf8') as fid:
-            fid.write(res.text)
-            logger.info(f'Config file "{path.name}" updated from {url}')
-    except requests.exceptions.ConnectionError:
-        logger.warning(f'Connection error. Could not update config file {path.name}')
-        raise
-
-
-def update_config_files() -> None:
-    """Downloads config files from github"""
-    for name in CONFIG_FILE_NAMES:
-        target_path = TEMP_CONFIG_DIRECTORY / name
-        update_config_file(target_path)
 
 
 @functools.cache
@@ -75,4 +47,5 @@ def get_translate_worms_object() -> "TranslateDyntaxa":
 
 
 if __name__ == '__main__':
-    update_config_files()
+    taxa = get_taxa_worms_object()
+    trans = get_translate_worms_object()
