0a8f76c82cecf65415cdcace99828e34b56f7956
Magnus Wenzer <magnus.wenzer@smhi.se>
2025-12-16 16:50:50 +0100

    Update config options and linting

--- src/nodc_worms/taxa_worms.py
+++ src/nodc_worms/taxa_worms.py
@@ -7,10 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 class TaxaWorms:
-
     def __init__(self, path: str | pathlib.Path, **kwargs):
         self._path = pathlib.Path(path)
-        self._encoding = kwargs.get('encoding', 'cp1252')
+        self._encoding = kwargs.get("encoding", "cp1252")
 
         self._header = []
         self._data = dict()
@@ -23,18 +22,27 @@ class TaxaWorms:
         return self._path
 
     @property
+    def source(self) -> str:
+        return self.path.name
+
+    @property
     def columns(self) -> list[str]:
         return sorted(self._df.columns)
 
     def _load_file(self) -> None:
-        self._df = pl.read_csv(self._path, separator='\t', encoding=self._encoding)
+        self._df = pl.read_csv(self._path, separator="\t", encoding=self._encoding)
 
     def get_aphia_id(self, scientific_name: str = None) -> str:
-        result = self._df.filter((pl.col('scientific_name') == scientific_name) &
-                                 (pl.col('status').is_in(['accepted', 'unassessed', 'unaccepted'])))['aphia_id']
+        # result = self._df.filter((pl.col('scientific_name') == scientific_name) &
+        #                          (pl.col('status').is_in(['accepted', 'unassessed',
+        #                          'unaccepted'])))['aphia_id']
+        result = self._df.filter(pl.col("scientific_name") == scientific_name)[
+            "aphia_id"
+        ]
         if not len(result):
-            return ''
+            return ""
         if len(result) > 1:
-            raise ValueError(f'Scientific name matches several AphiaID: {scientific_name}')
+            raise ValueError(
+                f"Scientific name matches several AphiaID: {scientific_name}"
+            )
         return result[0]
-
